cd95039e2eb26f9e5f9cf4d825f5df9b994a3166
chhayank2329 <68579147+chhayank2329@users.noreply.github.com>
2020-08-30 20:11:20 +0530

    o/p details

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -8,6 +8,11 @@ package execution;
 
 public class FrontDesk {
     public static void main(String[] args) {
-
+        System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-");
+        System.out.println("How may I help you today?");
+        System.out.println("Issue a new book for me.");
+        System.out.println("Return a previously issues book for me.");
+        System.out.println("Show me all my issues books");
+        System.out.println("Exit.");
     }
 }
