6da86d6ab0cc8492155b1e700d29c5baf6161465
chhayank2329 <68579147+chhayank2329@users.noreply.github.com>
2020-08-30 20:41:50 +0530

    Add constructor to Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -1,10 +1,15 @@
-/*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:49 PM
- *  File Name : Book.java
- * */
 package definitions;
 
+
 public class Book {
+    private String bookName;
+    private String authorName;
+    private String ISBNNumber;
+
+    public Book(String bookName, String authorName, String ISBNNumber) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.ISBNNumber = ISBNNumber;
+    }
 }
+
