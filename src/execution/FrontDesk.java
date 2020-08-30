dd06fd44c65bcfbf47f63ee0da723eb970932cc7
chhayank2329 <68579147+chhayank2329@users.noreply.github.com>
2020-08-30 20:54:00 +0530

    user name updated

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -1,29 +1,11 @@
 /*  Created by IntelliJ IDEA.
  *  User: Chhayank Sharma
  *  Date: 30/08/20
- *  Time: 8:23 PM
- *  File Name : FrontDesk.java
+ *  Time: 8:50 PM
+ *  File Name : Library.java
  * */
 package execution;
-
-import java.util.Scanner;
-
 public class FrontDesk {
     public static void main(String[] args) {
-        System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-");
-        System.out.println("How may I help you today?");
-        System.out.println("Issue a new book for me.");
-        System.out.println("Return a previously issues book for me.");
-        System.out.println("Show me all my issues books");
-        System.out.println("Exit.");
     }
-}
-
-class Library {
-    void add() {
-        System.out.println("Enter Book Name,Amount,ISBN Number");
-        Scanner book_details = new Scanner(System.in);
-
-    }
-
 }
