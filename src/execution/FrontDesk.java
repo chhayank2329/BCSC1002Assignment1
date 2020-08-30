21908fc98deae06c4234581d841f2d9bd7729e4a
chhayank2329 <68579147+chhayank2329@users.noreply.github.com>
2020-08-30 21:28:01 +0530

    import definitions.Student in  the FrontDesk.java and add field

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -6,10 +6,20 @@
  * */
 package execution;
 
+import definitions.Student;
+
 import java.util.Scanner;
 
 public class FrontDesk {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your details : ");
+        System.out.println("Name Of Student: ");
+        String nameOfTheStudent = scanner.nextLine();
+        System.out.println("University RollNo of Students: ");
+        long universityRollNumberOfStudent = scanner.nextLong();
+        System.out.println("Number of books issued: ");
+        int numberOfBooksIssuedByStudent = scanner.nextInt();
+        Student studentDetails = new Student(nameOfTheStudent, universityRollNumberOfStudent, numberOfBooksIssuedByStudent);
     }
 }
